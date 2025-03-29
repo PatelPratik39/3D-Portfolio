a354a6a9d0663504e06e968d8ab4989400c14918
PatelPratik39 <ptl.pratik717@gmail.com>
2025-03-29 18:37:04 -0500

    v

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 Built with React.js for handling the user interface, Three.js for rendering 3D elements, and styled with TailwindCSS, the 3D Minimalistic portfolio is a website project. The primary goal is to demonstrate the developer's skills in a unique manner that creates a lasting impact.
 
 ![Homepage Screenshot](./HomePage.png)
+![About Screenshot](./AboutPage.png)
 
 
 # ⚙️ Tech Stack
--- threejs_portfolio/src/App.jsx
+++ threejs_portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./sections/navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
+import Project from "./sections/Project";
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
         {" "}
         <Navbar />
         <Hero />
-        <About  />
+        <About />
+        <Project />
       </h1>
     </main>
   );
